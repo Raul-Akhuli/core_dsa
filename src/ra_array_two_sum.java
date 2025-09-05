9c2a11b5a9370aa1585d78ce4a53ab4dde8ca4a0
Raul <akhuliraul@gmail.com>
2025-09-05 09:01:26 +0530

    two sum complexity O(n) space complexity O(n)

--- src/ra_array_two_sum.java
+++ src/ra_array_two_sum.java
@@ -9,7 +9,7 @@ public class ra_array_two_sum {
         int n = sc.nextInt();
 
         System.out.println("enter the aray element");
-        int arr[] = new int[n];
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
